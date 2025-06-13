396b23d171f2016f6415c0cc8e9c6575c748fe15
vongalung <hutagalung.arthur@gmail.com>
2025-06-13 17:22:11 +0700

    Fixing
    - add return_status default init values
    - add more exception handling

--- src/main/java/com/test/library/main/repository/ReturnStatusRepo.java
+++ src/main/java/com/test/library/main/repository/ReturnStatusRepo.java
@@ -2,11 +2,13 @@ package com.test.library.main.repository;
 
 import com.test.library.main.model.ReturnStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 import java.util.stream.Stream;
 
 @Repository
 public interface ReturnStatusRepo extends JpaRepository<ReturnStatus, UUID> {
+    @Query("SELECT rs FROM ReturnStatus rs")
     Stream<ReturnStatus> streamAll();
 }
--- src/main/resources/data-postgresql.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
--- INITIALIZE master_setting
-INSERT INTO "testlibrary"."master_setting" ("id", "key", "value", "created_date")
-VALUES
-(gen_random_uuid(), 'checkout.return.days', '10', now());
-
-COMMIT;
